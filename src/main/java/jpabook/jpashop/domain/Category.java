cbbe7c60c3bbd344e2b996ef14dba92e9936c20d
sabsiru <zero013@naver.com>
2024-08-08 16:54:56 +0900

    지연로딩 설정

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,9 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
+
 @Entity
 public class Category {
     @Id @GeneratedValue
@@ -11,7 +14,7 @@ public class Category {
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
